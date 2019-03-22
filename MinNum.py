034fe52e182d78dd95f6429d7129bc70a263e5a7
priyas98 <48713960+priyas98@users.noreply.github.com>
2019-03-22 19:33:57 +0530

    Add files via upload

---
+++ MinNum.py
@@ -0,0 +1,13 @@
+class MinNum:
+    def __init__(self):
+        self.n=int(input())
+        self.narr=input()
+        self.narr=[int(i) for i in (self.narr).split(" ")]
+
+    def FindMin(self):
+        (self.narr).sort()
+        print((self.narr)[0])
+
+m=MinNum()
+m.FindMin()
+
