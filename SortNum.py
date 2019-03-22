db7d9d17574a12deab4987db4d4f2f8a85f33e29
priyas98 <48713960+priyas98@users.noreply.github.com>
2019-03-22 19:46:27 +0530

    Update SortNum.py

--- SortNum.py
+++ SortNum.py
@@ -5,7 +5,7 @@ class SortNum:
         self.narr=[int(i) for i in (self.narr).split(" ")]
 
     def PrintSorted(self):
-        (self.narr).sort()
+        self.narr.sort()
         for i in range(0,len(self.narr)):
             if(i==len(self.narr)-1):
                 print(self.narr[i])
