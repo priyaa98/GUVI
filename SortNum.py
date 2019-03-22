b5d7d22b17d39e40448a37b30fab418e8a8c0fb5
priyas98 <48713960+priyas98@users.noreply.github.com>
2019-03-22 19:40:46 +0530

    Add files via upload

---
+++ SortNum.py
@@ -0,0 +1,16 @@
+class SortNum:
+    def __init__(self):
+        self.n=int(input())
+        self.narr=input()
+        self.narr=[int(i) for i in (self.narr).split(" ")]
+
+    def PrintSorted(self):
+        (self.narr).sort()
+        for i in range(0,len(self.narr)):
+            if(i==len(self.narr)-1):
+                print(self.narr[i])
+            else:
+                print(self.narr[i],end=" ")
+
+p=SortNum()
+p.PrintSorted()
