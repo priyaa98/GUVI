a8cd4797fc1923275ffc80255fd9c129089cfdd7
priyas98 <48713960+priyas98@users.noreply.github.com>
2019-03-24 16:40:40 +0530

    Add files via upload

---
+++ FIndNotRepeated.py
@@ -0,0 +1,22 @@
+class FindNotRepeated:
+    def __init__(self):
+        self.n=int(input())
+        self.narr=input()
+
+    def ToFind(self):
+        narr=[int(i) for i in self.narr.split(" ")]
+        dict1={}
+        l=0
+        for i in narr:
+            dict1[i]=0
+        for i in narr:
+            dict1[i]+=1
+        for i in dict1:
+            if(dict1[i]==1):
+                l=i
+                break
+        print(l)        
+
+ft=FindNotRepeated()
+ft.ToFind()
+    
