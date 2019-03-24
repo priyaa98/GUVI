eef7dfc4fd4233fb81d8c4efba7fb359265dacb2
priyas98 <48713960+priyas98@users.noreply.github.com>
2019-03-24 16:14:09 +0530

    Add files via upload

---
+++ FindLargestCombination.py
@@ -0,0 +1,17 @@
+class LargestNumComb:
+    def __init__(self):
+        self.n=int(input())
+        self.narr=input()
+
+    def FindComb(self):
+        narr=self.narr.split(" ")
+        narr=[int(i) for i in narr]
+        narr.sort()
+        narr=[str(i) for i in narr]
+        narr="".join(narr)
+        narr=narr[::-1]
+        narr=int(narr)
+        print(narr)
+
+ln=LargestNumComb()
+ln.FindComb()
