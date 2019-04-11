763036319f8e05227f8d2eb4b7e2a2a95bf09545
priyas98 <48713960+priyas98@users.noreply.github.com>
2019-04-11 19:02:00 +0530

    Add files via upload

---
+++ FindingOneDiff.py
@@ -0,0 +1,16 @@
+class FindDiff:
+    def __init__(self):
+        self.str=input()
+        self.str=self.str.split(" ")
+    def finding(self):
+        count=0
+        for i in range(len(self.str[0])):
+            if(self.str[0][i]!=self.str[1][i]):
+                count+=1
+        if(count==1):
+            print("yes")
+        else:
+            print("no")
+
+fd=FindDiff()
+fd.finding()
